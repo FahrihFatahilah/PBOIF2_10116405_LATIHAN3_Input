2404a1b0ab5c2313d244eb476bf7220e003c716a
Fahrih Fatahilah <painchikusudo@gmail.com>
2020-10-20 12:17:08 +0800

    PBOIF2

---
+++ src/pboif2_10116405_latihan3/PBOIF2_10116405_LATIHAN3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10116405_latihan3;
+import java.util.Scanner;
+/**
+ *
+ * @author Agaaam
+ */
+public class PBOIF2_10116405_LATIHAN3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.print("Masukkan nama anda: ");
+        Scanner scanner = new Scanner(System.in);
+        
+        String nama = scanner.next();
+        System.out.println("Nama anda adalah " + nama);
+    }
+    
+}
